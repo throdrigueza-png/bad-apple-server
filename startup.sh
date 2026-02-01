8c5e067ec82b2d3236b6ff5f9838d4f7980d8a7f
throdrigueza-png <throdrigueza@unal.edu.co>
2026-02-01 10:09:34 -0500

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,5 +1,2 @@
 #!/bin/bash
-# Azure ya instala los requerimientos automáticamente si SCM_DO_BUILD_DURING_DEPLOYMENT está en 1.
-# Solo lanzamos el servidor.
-echo "--- ARRANCANDO SERVIDOR BAD APPLE EN PUERTO $WEBSITES_PORT ---"
 python3 main.py
